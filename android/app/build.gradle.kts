2d7e66a47b2165262e3276e511eb7d72979d3f70
Fanboynz <mp3geek@gmail.com>
2026-09-09 22:15:21 +1200

    build: testing build 472 / 353

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -26,7 +26,7 @@ android {
         applicationId = "com.noop.whoop"
         minSdk = 26
         targetSdk = 34
-        versionCode = 471
+        versionCode = 472
         versionName = "11.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
